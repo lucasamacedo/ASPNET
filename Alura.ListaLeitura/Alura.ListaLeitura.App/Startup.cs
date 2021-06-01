5cc86d663313bfd72b81111732834ed0523b7cbb
lucasamacedo <lucas_am_@hotmail.com>
2021-06-01 15:05:55 -0300

    Isolando o código de tratamento de requisicoes

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -17,16 +17,17 @@ namespace Alura.ListaLeitura.App
         {
             var _repo = new LivroRepositorioCSV();
 
-            var caminhosAtendidos = new Dictionary<string, string>
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
             {
-                { "/Livros/ParaLer", _repo.ParaLer.ToString() },
-                { "/Livros/Lendo", _repo.Lendo.ToString() },
-                { "/Livros/Lidos", _repo.Lidos.ToString() }
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos }
             };
             
             if (caminhosAtendidos.ContainsKey(context.Request.Path))
             {
-                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+                var f = caminhosAtendidos[context.Request.Path];
+                return f.Invoke(context);
             }
             context.Response.StatusCode = 404;
             return context.Response.WriteAsync("Caminho inexistente");
@@ -38,5 +39,19 @@ namespace Alura.ListaLeitura.App
 
             return context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }  
     }
 }
