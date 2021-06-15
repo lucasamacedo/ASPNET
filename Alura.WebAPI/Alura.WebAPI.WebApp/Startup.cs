81b25ea5f3f35eb1bf405f171385df5aeaec2231
lucasamacedo <lucas_am_@hotmail.com>
2021-06-15 13:18:23 -0300

    Separando o provedor de autenticacao

--- Alura.WebAPI/Alura.WebAPI.WebApp/Startup.cs
+++ Alura.WebAPI/Alura.WebAPI.WebApp/Startup.cs
@@ -50,24 +50,6 @@ namespace Alura.ListaLeitura.WebApp
                 options.OutputFormatters.Add(new LivroCsvFormatter());
             }).AddXmlSerializerFormatters();
             
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = "JwtBearer";
-                options.DefaultChallengeScheme = "JwtBearer";
-            }).AddJwtBearer("JwtBearer", options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")),
-                    ClockSkew = TimeSpan.FromMinutes(5),
-                    ValidIssuer = "Alura.WebApp",
-                    ValidAudience = "Postman",
-                };
-            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
