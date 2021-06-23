c08ed649e1b6d7a7f241fddbcc68852babd25290
lucasamacedo <lucas_am_@hotmail.com>
2021-06-23 10:21:01 -0300

    Capturando excecoes

--- Alura.WebAPI/Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ Alura.WebAPI/Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -68,7 +68,15 @@ namespace Alura.WebAPI.Api.Controllers
             if (ModelState.IsValid)
             {
                 var livro = model.ToLivro();
-                _repo.Incluir(livro);
+                try
+                {
+                    _repo.Incluir(livro);
+                }
+                catch (System.Exception e)
+                {
+                    var errorResponse = ErrorResponse.From(e);
+                    return StatusCode(500, errorResponse);
+                }
                 var uri = Url.Action("Recuperar", new { id = livro.Id });
                 return Created(uri, livro); // 201
             }
